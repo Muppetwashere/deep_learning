2cff6a52efc6ea247a46ae04c8a2f66c35610722
Alain Malige <alain.malige1@gmail.com>
2022-01-05 16:07:43 +0100

    working simple ex

--- mnist.py
+++ mnist.py
@@ -2,9 +2,12 @@ import torch
 import torchvision                                                       
 import torchvision.transforms as transforms
 import data
+import models
+import utils
 
 import os.path
 import torch.nn as nn
+
 
  ############################################################################################ Datasets
 
@@ -17,25 +20,12 @@ batch_size  = 128   # Using minibatches of 128 samples
 
 train_loader, valid_loader, test_loader = data.load_fashion_mnist(valid_ratio, batch_size, num_threads)
 
-
 print("The train set contains {} images, in {} batches".format(len(train_loader.dataset), len(train_loader)))
 print("The validation set contains {} images, in {} batches".format(len(valid_loader.dataset), len(valid_loader)))
 print("The test set contains {} images, in {} batches".format(len(test_loader.dataset), len(test_loader)))
 
 
-
-class LinearNet(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(LinearNet, self).__init__()
-        self.input_size = input_size
-        self.classifier = nn.Linear(self.input_size, num_classes)
-
-    def forward(self, x):
-        x = x.view(x.size()[0], -1)
-        y = self.classifier(x)
-        return y
-
-model = LinearNet(1*28*28, 10)
+model = models.LinearNet(1*28*28, 10)
 
 use_gpu = torch.cuda.is_available()
 if use_gpu:
@@ -49,98 +39,11 @@ model.to(device)
 f_loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters())
 
-def train(model, loader, f_loss, optimizer, device):
-    """
-    Train a model for one epoch, iterating over the loader
-    using the f_loss to compute the loss and the optimizer
-    to update the parameters of the model.
-
-    Arguments :
-
-        model     -- A torch.nn.Module object
-        loader    -- A torch.utils.data.DataLoader
-        f_loss    -- The loss function, i.e. a loss Module
-        optimizer -- A torch.optim.Optimzer object
-        device    -- a torch.device class specifying the device
-                     used for computation
-
-    Returns :
-    """
-
-    # We enter train mode. This is useless for the linear model
-    # but is important for layers such as dropout, batchnorm, ...
-    model.train()
-
-    for i, (inputs, targets) in enumerate(loader):
-        inputs, targets = inputs.to(device), targets.to(device)
-
-        # Compute the forward pass through the network up to the loss
-        outputs = model(inputs)
-        loss = f_loss(outputs, targets)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-def test(model, loader, f_loss, device):
-    """
-    Test a model by iterating over the loader
-
-    Arguments :
-
-        model     -- A torch.nn.Module object
-        loader    -- A torch.utils.data.DataLoader
-        f_loss    -- The loss function, i.e. a loss Module
-        device    -- The device to use for computation 
-
-    Returns :
-
-        A tuple with the mean loss and mean accuracy
-
-    """
-    # We disable gradient computation which speeds up the computation
-    # and reduces the memory usage
-    with torch.no_grad():
-        # We enter evaluation mode. This is useless for the linear model
-        # but is important with layers such as dropout, batchnorm, ..
-        model.eval()
-        N = 0
-        tot_loss, correct = 0.0, 0.0
-        for i, (inputs, targets) in enumerate(loader):
-
-            # We got a minibatch from the loader within inputs and targets
-            # With a mini batch size of 128, we have the following shapes
-            #    inputs is of shape (128, 1, 28, 28)
-            #    targets is of shape (128)
-
-            # We need to copy the data on the GPU if we use one
-            inputs, targets = inputs.to(device), targets.to(device)
-
-            # Compute the forward pass, i.e. the scores for each input image
-            outputs = model(inputs)
-
-            # We accumulate the exact number of processed samples
-            N += inputs.shape[0]
-
-            # We accumulate the loss considering
-            # The multipliation by inputs.shape[0] is due to the fact
-            # that our loss criterion is averaging over its samples
-            tot_loss += inputs.shape[0] * f_loss(outputs, targets).item()
-
-            # For the accuracy, we compute the labels for each input image
-            # Be carefull, the model is outputing scores and not the probabilities
-            # But given the softmax is not altering the rank of its input scores
-            # we can compute the label by argmaxing directly the scores
-            predicted_targets = outputs.argmax(dim=1)
-            correct += (predicted_targets == targets).sum().item()
-        return tot_loss/N, correct/N
-
 epochs = 10
 
 for t in range(epochs):
     print("Epoch {}".format(t))
-    train(model, train_loader, f_loss, optimizer, device)
+    utils.train(model, train_loader, f_loss, optimizer, device)
 
-    val_loss, val_acc = test(model, valid_loader, f_loss, device)
+    val_loss, val_acc = utils.test(model, valid_loader, f_loss, device)
     print(" Validation : Loss : {:.4f}, Acc : {:.4f}".format(val_loss, val_acc))
