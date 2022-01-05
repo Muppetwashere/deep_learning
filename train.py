39d1db972bcb06495427092aa2e628ab104bd8c4
Alain Malige <alain.malige1@gmail.com>
2022-01-05 15:56:57 +0100

    simple version

--- train.py
+++ train.py
@@ -18,15 +18,6 @@ if __name__ == '__main__':
     else:
         device = torch.device('cpu')
 
-    # Where to store the logs
-    logdir = utils.generate_unique_logpath("./logs", model)
-    print("Logging to {}".format(logdir))
-
-    if not os.path.exists("./logs"):
-        os.mkdir("./logs")
-    if not os.path.exists(logdir):
-        os.mkdir(logdir)
-    
     # load the dataset
     num_threads = 4 # Loading the dataset is using 4 CPU threads
     valid_ratio = 0.2 # Going to use 80%/20% split for train/valid
@@ -39,8 +30,6 @@ if __name__ == '__main__':
     loss = nn.CrossEntropyLoss()  # This computes softmax internally
     optimizer = torch.optim.Adam(model.parameters())
 
-    # Where to save the logs of the metrics
-    tensorboard_writer   = SummaryWriter(log_dir = logdir)
 
     ######## Main Loop
     epochs = 10
@@ -54,11 +43,3 @@ if __name__ == '__main__':
 
         test_loss, test_acc = utils.test(model, test_loader, loss, device)
         print(" Test       : Loss : {:.4f}, Acc : {:.4f}".format(test_loss, test_acc))
-
-        utils.model_checkpoint.update(val_loss)
-        tensorboard_writer.add_scalar('metrics/train_loss', train_loss, t)
-        tensorboard_writer.add_scalar('metrics/train_acc',  train_acc, t)
-        tensorboard_writer.add_scalar('metrics/val_loss', val_loss, t)
-        tensorboard_writer.add_scalar('metrics/val_acc',  val_acc, t)
-        tensorboard_writer.add_scalar('metrics/test_loss', test_loss, t)
-        tensorboard_writer.add_scalar('metrics/test_acc',  test_acc, t)
