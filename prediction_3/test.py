1eb197cd1a86bec216bfd86ac1d97288b769869a
Alain Malige <alain.malige1@gmail.com>
2022-01-24 20:23:57 +0100

    print

--- prediction_3/test.py
+++ prediction_3/test.py
@@ -38,6 +38,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Load dataset
 plankton_dataset = PlanktonDataset(DATA_TRAIN_PATH, final_size = img_size)
 
+
 # Split dataset
 nb_train = int(train_ratio * len(plankton_dataset))+1
 nb_eval = int(eval_ratio * len(plankton_dataset))
@@ -53,22 +54,26 @@ print("The train set contains {} images, in {} batches".format(len(train_dataloa
 print("The evaluation set contains {} images, in {} batches".format(len(eval_dataloader.dataset), len(eval_dataloader)))
 print("The test set contains {} images, in {} batches".format(len(test_dataloader.dataset), len(test_dataloader)))
 
+# example
 example = iter(train_dataloader)
 img,label = example.next()
-print(label)
 
 
-#plt.imshow(imgs[0],vmin=0, vmax=1.0, cmap=cm.gray)
-#nsamples=10
-#classes_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal','Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# classes names
+classes = []
+subfolders = glob.glob(DATA_TRAIN_PATH + '/*')
+subfolders.sort()
+for subfolder in subfolders:
+    classes.append(subfolder.split('/')[-1])
 
-#fig=plt.figure(figsize=(20,5),facecolor='w')
-#for i in range(nsamples):
-#    ax = plt.subplot(1,nsamples, i+1)
-#    plt.imshow(imgs[i, 0, :, :], vmin=0, vmax=1.0, cmap=cm.gray)
-#    ax.set_title("{}".format(classes_names[labels[i]]), fontsize=15)
-#    ax.get_xaxis().set_visible(False)
-#    ax.get_yaxis().set_visible(False)
+nsamples=10
+fig=plt.figure(figsize=(20,5),facecolor='w')
+for i in range(nsamples):
+    ax = plt.subplot(1,nsamples, i+1)
+    plt.imshow(img[i, 0, :, :], vmin=0, vmax=1.0)
+    ax.set_title("{}".format(classes[label[i]]), fontsize=15)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
 #plt.savefig('fashionMNIST_samples.png', bbox_inches='tight')
 #plt.show()
